415a4705ef194ff2c55037d176471087c20a3f8a
AineeJames <olsenaiden33@gmail.com>
2022-12-02 17:26:09 -0800

    better comments

--- src/LilyPad4G/LilyPad4G.ino
+++ src/LilyPad4G/LilyPad4G.ino
@@ -1,75 +1,50 @@
 /**
- *
+ * LilyPad4G.ino
  * This project uses the VEML7700, DS18B20, and SHT31 sensor to log environment data and logs it to both the SD card and also MQTT/MongoDB
- * 
  */
 #include "arduino_secrets.h"
-
+// Loom includes
 #include <Loom_Manager.h>
 #include <Hardware/Loom_Hypnos/Loom_Hypnos.h>
-
+// Sensor Inlcudes
 #include <Sensors/Loom_Analog/Loom_Analog.h>
 #include <Sensors/I2C/Loom_SHT31/Loom_SHT31.h>
-
+// Internet/DB Includes
 #include <Internet/Logging/Loom_MQTT/Loom_MQTT.h>
 #include <Internet/Connectivity/Loom_LTE/Loom_LTE.h>
 
-
-Manager manager("Lilypad4G", 1);
-
-// Create a new Hypnos object
-Loom_Hypnos hypnos(manager, HYPNOS_VERSION::V3_3, TIME_ZONE::PST, true);
-// Analog for reading battery voltage
-Loom_Analog analog(manager);
+Manager manager("Lilypad4G", 1); //Establish the Loom manager first
+Loom_Hypnos hypnos(manager, HYPNOS_VERSION::V3_3, TIME_ZONE::PST, true); // Create a new Hypnos object
+Loom_Analog analog(manager); // Analog for reading battery voltage
 
 // Create sensor classes
 Loom_SHT31 sht(manager);
 
+// Create lte and mqtt classes
 Loom_LTE lte(manager, NETWORK_APN, NETWORK_USER, NETWORK_PASS);
 Loom_MQTT mqtt(manager, lte.getClient(), SECRET_BROKER, SECRET_PORT, DATABASE, BROKER_USER, BROKER_PASS);
 
-// Called when the interrupt is triggered 
+// Called when the RTC interrupt is triggered 
 void isrTrigger(){
   hypnos.wakeup();
 }
 
 void setup() {
-
-  // Wait 20 seconds for the serial console to open
-  manager.beginSerial();
-
-  // Enable the hypnos rails
-  hypnos.enable();
-
-  // Initialize all in-use modules
-  manager.initialize();
-
-  // Register the ISR and attach to the interrupt
-  hypnos.registerInterrupt(isrTrigger);
+  manager.beginSerial();                // Wait 20 seconds for the serial console to open
+  hypnos.enable();                      // Enable the hypnos rails
+  manager.initialize();                 // Initialize all in-use modules
+  hypnos.registerInterrupt(isrTrigger); // Register the ISR and attach to the interrupt
 }
 
 void loop() {
+  manager.measure();       // Measure sensor values
+  manager.package();       // Package data from measurments
+  manager.display_data();  // Print the current JSON packet                      
+  hypnos.logToSD();        // Log the data to the SD card              
+  mqtt.publish();          // Publish the collected data to MQTT
 
-  // Measure and package the data
-  manager.measure();
-  manager.package();
-  
-  // Print the current JSON packet
-  manager.display_data();            
-
-  // Log the data to the SD card              
-  hypnos.logToSD();
-
-  // Publish the collected data to MQTT
-  mqtt.publish();
-
-  // Set the RTC interrupt alarm to wake the device in 15 min
-  hypnos.setInterruptDuration(TimeSpan(0, 0, 0, 10));
-
-  // Reattach to the interrupt after we have set the alarm so we can have repeat triggers
+  hypnos.setInterruptDuration(TimeSpan(0, 0, 1, 0)); // Interrupt every 1 minute
   hypnos.reattachRTCInterrupt();
-  
-  // Put the device into a deep sleep, operation HALTS here until the interrupt is triggered
   hypnos.sleep();
   
 }

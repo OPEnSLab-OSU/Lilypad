a4d6a923517698471954cdba12fcd87295ae150e
Jace Palmer <60857104+jamopopper@users.noreply.github.com>
2022-12-05 11:40:21 -0800

    Update LilyPad4G.ino
    update to work with new temp sensor

--- src/LilyPad4G/LilyPad4G.ino
+++ src/LilyPad4G/LilyPad4G.ino
@@ -13,6 +13,9 @@
 #include <Internet/Logging/Loom_MQTT/Loom_MQTT.h>
 #include <Internet/Connectivity/Loom_LTE/Loom_LTE.h>
 
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
 Manager manager("Lilypad4G", 1); //Establish the Loom manager first
 Loom_Hypnos hypnos(manager, HYPNOS_VERSION::V3_3, TIME_ZONE::PST, true); // Create a new Hypnos object
 Loom_Analog analog(manager); // Analog for reading battery voltage
@@ -24,6 +27,9 @@ Loom_SHT31 sht(manager);
 Loom_LTE lte(manager, NETWORK_APN, NETWORK_USER, NETWORK_PASS);
 Loom_MQTT mqtt(manager, lte.getClient(), SECRET_BROKER, SECRET_PORT, DATABASE, BROKER_USER, BROKER_PASS);
 
+OneWire oneWire(9);
+DallasTemperature sensors(&oneWire);
+
 // Called when the RTC interrupt is triggered 
 void isrTrigger(){
   hypnos.wakeup();
@@ -34,11 +40,14 @@ void setup() {
   hypnos.enable();                      // Enable the hypnos rails
   manager.initialize();                 // Initialize all in-use modules
   hypnos.registerInterrupt(isrTrigger); // Register the ISR and attach to the interrupt
+  sensors.begin();
 }
 
 void loop() {
   manager.measure();       // Measure sensor values
+  sensors.requestTemperatures();
   manager.package();       // Package data from measurments
+  manager.addData("Temperature", "Celcius", sensors.getTempCByIndex(0));
   manager.display_data();  // Print the current JSON packet                      
   hypnos.logToSD();        // Log the data to the SD card              
   mqtt.publish();          // Publish the collected data to MQTT

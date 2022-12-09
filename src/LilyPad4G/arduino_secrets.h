4bcf29a7dde10b6b407d047b78b27486d0a3d640
Aiden Olsen <71784728+AineeJames@users.noreply.github.com>
2022-12-08 18:50:32 -0800

    Delete arduino_secrets.h

--- src/LilyPad4G/arduino_secrets.h
+++
@@ -1,9 +0,0 @@
-#define NETWORK_APN "hologram"
-#define NETWORK_USER ""
-#define NETWORK_PASS ""
-
-#define SECRET_BROKER "cas-mosquitto.biossys.oregonstate.edu"
-#define SECRET_PORT 1883
-#define DATABASE "LilyPadV1"
-#define BROKER_USER "User"
-#define BROKER_PASS "password"
